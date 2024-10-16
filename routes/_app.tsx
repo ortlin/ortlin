cc2bf506c345dd6cac1c982bc88136ab0055db1f
rahulspace <rahultp518@gmail.com>
2024-10-16 11:18:48 +0530

    Use dm sans font

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,8 +7,18 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>numfray</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body>
+      <body class="font-sans">
         <Component />
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,11 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', "sans-serif"],
+      },
+    },
+  },
 } satisfies Config;
