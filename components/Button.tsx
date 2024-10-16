8c3f43a8e20b921e5ffd5239dd8e6e6f5f649f1b
rahulspace <rahultp518@gmail.com>
2024-10-16 12:52:41 +0530

    Update button with icon

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,23 @@
 import { JSX } from "preact";
+import { LucideIcon } from "lucide-preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+import Icon from "./Icon.tsx";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+interface Props extends JSX.HTMLAttributes<HTMLButtonElement> {
+  Icon: LucideIcon;
+  strokeColor: string;
+  textColor: string;
+}
+
+export function Button(props: Props) {
   return (
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
-    />
+      class={`px-3 py-2.5 flex gap-2 rounded-xl border text-sm items-center justify-center font-semibold ${props.strokeColor} ${props.textColor}`}
+    >
+      <Icon Icon={props.Icon} size={17} />
+      {props.children}
+    </button>
   );
 }
