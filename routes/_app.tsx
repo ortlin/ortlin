dccd308776200a14137a9640a2d7fb6fca2db3fd
renjithspace <renjithrajeevvk@gmail.com>
2024-10-16 16:31:01 +0530

    Update title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,9 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>numfray</title>
+        <title>
+          Numfray - GUI for OpenAI API
+        </title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
