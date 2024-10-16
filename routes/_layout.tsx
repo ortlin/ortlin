72bb4a36ae63be179d75d8ed5ef0e6a3ecf2360e
renjithspace <renjithrajeevvk@gmail.com>
2024-10-16 16:44:32 +0530

    Add link to github and opencollective buttons

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -3,30 +3,19 @@ import { Button } from "../components/Button.tsx";
 import { Github, Heart, KeyRound } from "lucide-preact";
 
 export default function Layout(props: PageProps) {
-  const handleAddAPIKeyClick = () => {
-    // TODO
-  };
-
-  const handleContributeClick = () => {
-    // TODO
-  };
-
-  const handleDonateClick = () => {
-    // TODO
-  };
-
   return (
     <div class="flex h-screen">
       <div class="flex flex-col bg-slate-900 w-72">
         <div class="px-8 py-5 border-b border-slate-700">
-          <h1 class="text-slate-100 font-semibold text-xl">Numfray</h1>
+          <h1 class="text-slate-100 font-semibold text-xl">
+            <a href="/">Numfray</a>
+          </h1>
         </div>
         <div class="flex-grow">
         </div>
         <div class="mb-6 mx-auto">
           <Button
             Icon={KeyRound}
-            onClick={handleAddAPIKeyClick}
             strokeColor="border-slate-700"
             textColor="text-slate-100"
           >
@@ -38,17 +27,17 @@ export default function Layout(props: PageProps) {
         <div class="border-b border-slate-700 h-[69px] flex flex-row items-center justify-end px-7 gap-4">
           <Button
             Icon={Github}
-            onClick={handleContributeClick}
             strokeColor="border-slate-600"
             textColor="text-slate-100"
+            link="https://github.com/numfray/numfray"
           >
             Contribute to project
           </Button>
           <Button
             Icon={Heart}
-            onClick={handleDonateClick}
             strokeColor="border-cyan-800"
             textColor="text-cyan-300"
+            link="https://opencollective.com/numfray/donate"
           >
             Donate to our collective
           </Button>
